747e97309d78bf43e414058b69f5f0467787bdf7
Rawan Malek <rawanmalek@hotmail.co.uk>
2022-09-09 18:16:37 +0100

    working halfway through flight service

--- src/main/java/com/example/Airline_api_lab/services/FlightService.java
+++ src/main/java/com/example/Airline_api_lab/services/FlightService.java
@@ -1,4 +1,25 @@
 package com.example.Airline_api_lab.services;
 
+import com.example.Airline_api_lab.models.Flight;
+import com.example.Airline_api_lab.repositories.FlightRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class FlightService {
+
+    @Autowired
+    FlightRepository flightRepository;
+
+    public List<Flight> getAllFlights(){ return flightRepository.findAll();}
+
+    public Optional<Flight> getFlightById(Long id){ return flightRepository.findById(id);}
+
+    public Flight addNewFlight(Flight flight){
+        flightRepository.save(flight);
+        return flight;
+    }
 }
