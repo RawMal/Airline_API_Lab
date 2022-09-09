5588f18942e89c57807d4e05a1b88ee10f3a4910
Rawan Malek <rawanmalek@hotmail.co.uk>
2022-09-09 18:04:53 +0100

    completed repositories

--- src/main/java/com/example/Airline_api_lab/repositories/FlightRepository.java
+++ src/main/java/com/example/Airline_api_lab/repositories/FlightRepository.java
@@ -1,4 +1,9 @@
 package com.example.Airline_api_lab.repositories;
 
-public interface FlightRepository {
+import com.example.Airline_api_lab.models.Flight;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FlightRepository extends JpaRepository<Flight, Long> {
 }
--- src/main/java/com/example/Airline_api_lab/repositories/PassengerRepository.java
+++ src/main/java/com/example/Airline_api_lab/repositories/PassengerRepository.java
@@ -1,4 +1,9 @@
 package com.example.Airline_api_lab.repositories;
 
-public interface PassengerRepository {
+import com.example.Airline_api_lab.models.Passenger;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PassengerRepository extends JpaRepository<Passenger, Long> {
 }
