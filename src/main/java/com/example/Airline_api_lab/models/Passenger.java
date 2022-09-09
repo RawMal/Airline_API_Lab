f43d5fda229a763b9ff8b5214665280a62c323c6
Rawan Malek <rawanmalek@hotmail.co.uk>
2022-09-09 11:22:27 +0100

    Flight model completed

--- src/main/java/com/example/Airline_api_lab/models/Passenger.java
+++ src/main/java/com/example/Airline_api_lab/models/Passenger.java
@@ -1,5 +1,15 @@
 package com.example.Airline_api_lab.models;
 
-@Entity
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "passengers")
 public class Passenger {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id
+
 }
