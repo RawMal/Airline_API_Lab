43b9ec256a2539b6ae953bfe31d89768181379ff
Rawan Malek <rawanmalek@hotmail.co.uk>
2022-09-10 10:44:14 +0100

    completed passenger service, continue with flight service

--- src/main/java/com/example/Airline_api_lab/services/PassengerService.java
+++ src/main/java/com/example/Airline_api_lab/services/PassengerService.java
@@ -1,4 +1,25 @@
 package com.example.Airline_api_lab.services;
 
+import com.example.Airline_api_lab.models.Passenger;
+import com.example.Airline_api_lab.repositories.PassengerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class PassengerService {
+
+    @Autowired
+    PassengerRepository passengerRepository;
+
+    public List<Passenger> getAllPassengers(){ return passengerRepository.findAll();}
+
+    public Optional<Passenger> getPassengerById(Long id){ return passengerRepository.findById(id).get();}
+
+    public Passenger addNewPassenger(Passenger passenger){
+        passengerRepository.save(passenger);
+        return passenger;
+    }
 }
